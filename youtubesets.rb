3fc2cb3ad13862f66412ad187ef5cbc62dca39ca
caseywatts <casey.s.watts@gmail.com>
2014-03-01 13:10:45 -0500

    abort video urls directly

--- youtubesets.rb
+++ youtubesets.rb
@@ -11,16 +11,16 @@ helpers do
   def randomvideo(set)
     set.sample
   end
-  def embedyoutube(videourl)
+  def embedyoutube(videonumber)
     '<body style="margin:0;">' + \
-    '<object height="100%" width="100%"><param name="movie" value="' + videourl + '&autoplay=1" /><embed height="100%" src="' + videourl + '&autoplay=1" type="application/x-shockwave-flash" width="100%"></embed></object>' + \
+    '<object height="100%" width="100%"><param name="movie" value="http://www.youtube.com/v/' + videonumber + '&autoplay=1" /><embed height="100%" src="http://www.youtube.com/v/' + videonumber + '&autoplay=1" type="application/x-shockwave-flash" width="100%"></embed></object>' + \
     '</body>'
   end
   def beyoncevideos
     ["QczgvUDskk0", "VBmMU_iwe6U", "Vjw92oUduEM", "4m1EFMoRFvY", "FHp2KgyQUFk"]
   end
   def pmjvideos
-    ["www.youtube.com/watch?v=pXYWDtXbBB0", "www.youtube.com/watch?v=VBmCJEehYtU", "www.youtube.com/watch?v=GZQJrM09jbU"]
+    ["pXYWDtXbBB0", "VBmCJEehYtU", "GZQJrM09jbU"]
   end
 end
 
