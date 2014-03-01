8fcb7584c892c61d0777d35c44f7c14de44c000b
caseywatts <casey.s.watts@gmail.com>
2014-03-01 13:20:35 -0500

    text block instead of concatenated strings

--- youtubesets.rb
+++ youtubesets.rb
@@ -12,9 +12,11 @@ helpers do
     set.sample
   end
   def embedyoutube(videonumber)
-    '<body style="margin:0;">' + \
-    '<object height="100%" width="100%"><param name="movie" value="http://www.youtube.com/v/' + videonumber + '&autoplay=1" /><embed height="100%" src="http://www.youtube.com/v/' + videonumber + '&autoplay=1" type="application/x-shockwave-flash" width="100%"></embed></object>' + \
-    '</body>'
+    %{
+    <body style="margin:0;">
+    <object height="100%" width="100%"><param name="movie" value="http://www.youtube.com/v/#{videonumber}&autoplay=1" /><embed height="100%" src="http://www.youtube.com/v/#{videonumber}&autoplay=1" type="application/x-shockwave-flash" width="100%"></embed></object>
+    </body>
+    }
   end
   def beyoncevideos
     ["QczgvUDskk0", "VBmMU_iwe6U", "Vjw92oUduEM", "4m1EFMoRFvY", "FHp2KgyQUFk"]
@@ -27,14 +29,11 @@ end
 ##INDEX
 ##Main Welcome Page
 get '/' do
-  "<h1>Hi!</h1><h2>If you know the url you can play a random video from set of youtube videos!</h2>" + \
-    "<a href='set/new'>New Set</a>"
-
+  "<h1>Hi!</h1><h2>If you know the url you can play a random video from set of youtube videos!</h2>"
 end
 
 ##Display the Video Sets
 get '/beyonce' do
-  #@@beyonce.to_s
   embedyoutube(randomvideo(beyoncevideos))
 end
 
@@ -48,8 +47,12 @@ get '/session' do
   session.inspect
 end
 
+##example of parameters
 get '/params/:idlol' do
   params.inspect
 end
 
+get '/favorite/:fruit' do |fruit|
+  "My favorite fruit is the " + fruit.to_s
+end
 
